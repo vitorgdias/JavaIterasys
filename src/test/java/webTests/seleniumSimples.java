41875580d4aac6d9efde0d03701e85a590853867
vitorgdias <vitorgalves@live.com>
2023-02-27 11:10:18 -0300

    Adicionada consulta de dois produtos dentro do site '(RO e PW)'

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,14 +17,21 @@ public class seleniumSimples {
     // 3.1 - Atributos
     WebDriver driver;       // Declarar o objetodo Selenium WebDriver
     // 3.2 - Métodos e funções
-    @Test
-    public void consultarSite(){
+    @BeforeMethod //Se utilizar Test ao invés de Method, o programa reutiliza a mesma janela já aberta.
+    public void iniciar(){
         // A - Início
         System.setProperty("webdriver.chrome.driver","drivers/chrome/109/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1768, 992));
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
-
+    }
+    @AfterMethod
+    public void finalizar(){
+        // C - Encerrar o processo
+        driver.quit(); //Encerra o objeto do Selenium
+    }
+    @Test(priority = 1)
+    public void consultarRO(){
         // B - Realizar o teste
         driver.get("https://hype.games/br"); //Abre o site que foi informado
         driver.findElement(By.id("search-input")).click();                        //Clica no campo de pesquisa
@@ -33,11 +40,22 @@ public class seleniumSimples {
         driver.findElement(By.id("global-search__btn")).click();
         driver.findElement(By.cssSelector("div.w-100.cart")).click();
 
-        assertEquals(driver.findElement(By.cssSelector("span.item-price__current")).getText(),"R$ 19.90");
-        assertEquals(driver.findElement(By.cssSelector("info")).getText(),"Ragnarök - Pacote de 11.000 ROPS");
-        driver.findElement(By.cssSelector("a.next")).click();
+        //assertEquals(driver.findElement(By.cssSelector("span.item-price__current")).getText(),"R$ 19.90");
+        //assertEquals(driver.findElement(By.cssSelector("info")).getText(),"Ragnarök - Pacote de 11.000 ROPS");
+        //driver.findElement(By.cssSelector("a.next")).click();
+    }
+    @Test(priority = 2,dependsOnMethods = {"consultarRO"})
+    public void consultarPW(){
+        // B - Realizar o teste
+        driver.get("https://hype.games/br");
+        driver.findElement(By.id("search-input")).click();
+        driver.findElement(By.id("search-input")).clear();
+        driver.findElement(By.id("search-input")).sendKeys("perfect world");
+        driver.findElement(By.id("global-search__btn")).click();
+        driver.findElement(By.cssSelector("div.w-100.cart")).click();
 
-
-        driver.quit(); //Encerra o objeto do Selenium
+        //assertEquals(driver.findElement(By.cssSelector("span.item-price__current")).getText(),"R$ 219.90");
+        //assertEquals(driver.findElement(By.cssSelector("a.info-box.h3")).getText(),"Perfect World – Pacote de 46.200 GOLD – Melhor Oferta");
+        //driver.findElement(By.cssSelector("a.next")).click();
     }
 }
