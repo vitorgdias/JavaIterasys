404432e065a83fc928821474a8898812ef844b45
vitorgdias <vitorgalves@live.com>
2023-05-10 14:08:41 -0300

    Criado o tempo de espera dinâmico, para identificar os elementos da página e prosseguir com o testes.

--- src/test/java/webTests/comprarCursoCS.java
+++ src/test/java/webTests/comprarCursoCS.java
@@ -8,7 +8,10 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,12 +19,15 @@ import static org.testng.Assert.assertEquals;
 
 public class comprarCursoCS {
     WebDriver driver;
+    WebDriverWait wait;
     @Before
     public void iniciar(){
         System.setProperty("webdriver.chrome.driver","drivers/chrome/112/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        //driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        //Recomenda-se usar apenas um tipo de espera no código
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver,60); //Espera até 60 segundos
         System.out.println("0 - Antes do Teste iniciar");
     }
     @After
@@ -39,7 +45,6 @@ public class comprarCursoCS {
     public void clicoEmCursos() throws InterruptedException {
         driver.findElement(By.id("16237702146520")).click();
         System.out.println("2 - Clicou em cursos");
-        Thread.sleep(600);
     }
 
     @And("^pesquiso por \"([^\"]*)\"$")
@@ -53,7 +58,10 @@ public class comprarCursoCS {
 
     @And("^visualizo a lista de resultados para \"([^\"]*)\"$")
     public void visualizoAListaDeResultadosPara(String curso) throws InterruptedException {
-        Thread.sleep(600);
+        // Thread.sleep(900); Evitar usar isso, pois é uma pausa forçada da automação
+        String textoEsperado = curso;
+        WebElement h3texto = driver.findElement(By.cssSelector("h3"));
+        wait.until(ExpectedConditions.textToBePresentInElement(h3texto,textoEsperado));
         System.out.println("4 - Exibiu a lista de resultados da pesquisa para o curso " + curso);
     }
 
@@ -65,6 +73,9 @@ public class comprarCursoCS {
 
     @Then("^confirmo o nome como \"([^\"]*)\" e o preco de \"([^\"]*)\"$")
     public void confirmoONomeComoEOPrecoDe(String curso, String preco){
+        String textoEsperado = curso;
+        WebElement h3texto = driver.findElement(By.cssSelector("h3"));
+        wait.until(ExpectedConditions.textToBePresentInElement(h3texto,textoEsperado));
         assertEquals(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/main/div[1]/div/section/div/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/p")).getText(),preco);
         assertEquals(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/main/div[1]/div/section/div/div[1]/div/div[1]/div/div[1]/div/h1")).getText(),"Introdução ao " + curso);
         System.out.println("6 - Confirmou o nome como " + curso + " e preco do curso como " + preco);
--- src/test/resources/features/comprarCursoCS.feature
+++ src/test/resources/features/comprarCursoCS.feature
@@ -6,3 +6,15 @@ Feature: Comprar Curso CS
     Then visualizo a lista de resultados para "Python"
     When clico em matricule-se
     Then confirmo o nome como "Python" e o preco de "R$ 27,90"
+
+  Scenario Outline: Pesquisar Curso e Incluir no Carrinho
+    Given que acesso o site da Iterasys
+    When clico em cursos
+    And pesquiso por <curso>
+    Then visualizo a lista de resultados para <curso>
+    When clico em matricule-se
+    Then confirmo o nome como <curso> e o preco de <preco>
+    Examples:
+      | curso     | preco      |
+      | "Python"  | "R$ 27,90" |
+      | "ao Java" | "R$ 27,90" |
